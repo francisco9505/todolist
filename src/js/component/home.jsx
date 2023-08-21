74c5ed536dbb6a44929777224bd9bbc293e4692b
francisco9505 <franciscocastillo9505@gmail.com>
2023-08-21 17:02:28 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todos, setTodos] = useState([]);
+
+  const todoList = (event) => {
+    if (event.key === "Enter") {
+      setTodos([
+        ...todos,
+        { value: event.target.value, id: new Date().getTime() },
+      ]);
+      console.log({ value: event.target.value, id: new Date().getTime() });
+      event.target.value = "";
+    }
+  };
+
+  const deleteTask = (task) => {
+    setTodos(todos.filter((i) => i.id !== task.id));
+  };
+
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems:'center',
+      }}
+    >
+      <h1 style={{fontSize:'100px',fontWeight:'100',color:'pink',opacity:'0.6'}}>todos</h1>
+      <input type="text" onKeyDown={todoList} />
+      <ul>
+        {todos.map((task) => (
+          <li key={task.id}>
+            {task.value} <button onClick={() => deleteTask(task)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
